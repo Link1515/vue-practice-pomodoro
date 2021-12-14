cd28fa12d06226b5af0edea379b2e296679658d4
Link1515 <88765055+Link1515@users.noreply.github.com>
2021-12-14 00:21:46 +0000

    Deploying to gh-pages from @ Link1515/vue-practice-pomodoro@7302a59d5a49302e65006685daff4976f3c8d764 🚀

---
+++ precache-manifest.8d5cd1c4c7f8266b9637ad4d82481d98.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "120b8615100faeebcf57",
+    "url": "css/app.deb39008.css"
+  },
+  {
+    "revision": "69d81e8932d41732ab0b",
+    "url": "css/chunk-vendors.989680b1.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "dea9842b1b34def05c94e10c636cddbb",
+    "url": "img/tomato.png"
+  },
+  {
+    "revision": "ed0c09f833d3e59345f6102bddef6470",
+    "url": "index.html"
+  },
+  {
+    "revision": "120b8615100faeebcf57",
+    "url": "js/app.0ebcbdbc.js"
+  },
+  {
+    "revision": "69d81e8932d41732ab0b",
+    "url": "js/chunk-vendors.0f8ae757.js"
+  },
+  {
+    "revision": "8cc4b6cae96d08e636b3",
+    "url": "js/list.3b4feebc.js"
+  },
+  {
+    "revision": "a3a14fee12fc762907d4",
+    "url": "js/notfound.f0f96cf7.js"
+  },
+  {
+    "revision": "5bdcf5673bf5adba798b",
+    "url": "js/settings.5bb1cc87.js"
+  },
+  {
+    "revision": "800b623de8c7ccf99631f1adcc64fa89",
+    "url": "manifest.json"
+  },
+  {
+    "revision": "965b614dd8fb0cb17e9eac0f0ace01fb",
+    "url": "media/alarm.965b614d.mp3"
+  },
+  {
+    "revision": "46c744f5e53a2259c87f4b029cb43e03",
+    "url": "media/yay.46c744f5.mp3"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "robots.txt"
+  }
+]);
